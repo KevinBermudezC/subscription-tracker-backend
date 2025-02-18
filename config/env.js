1c7c6af73b8861795dc5c9473cc0fb67d2657a68
KevinBermudezC <kevinbermudezc@gmail.com>
2025-02-17 20:33:12 -0500

    add MongoDB and Mongoose integration with database connection setup

--- config/env.js
+++ config/env.js
@@ -7,6 +7,7 @@ config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
  * 
  * @constant {string} PORT - The port number on which the server will listen.
  * @constant {string} NODE_ENV - The environment in which the server is running (e.g., development, production).
+ * @constant {string} DB_URI - The URI of the MongoDB database.
 */
 
-export const { PORT, NODE_ENV } = process.env;
+export const { PORT, NODE_ENV, DB_URI } = process.env;
